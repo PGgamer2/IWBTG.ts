ca92cddabd8566f78f8ec5ed72d552393fe79e3c
PGgamer2 <38359416+PGgamer2@users.noreply.github.com>
2022-09-11 10:24:18 +0200

    Use one collision box for multiple tiles

--- src/levels/TestLevel.ts
+++ src/levels/TestLevel.ts
@@ -1,23 +1,17 @@
 import { BasicLevel } from './BasicLevel';
 import { PlayerObject } from '../objects/player/PlayerObject';
-import { ImageObject } from '../objects/ImageObject';
 import { SpikeObject } from '../objects/SpikeObject';
 import { AudioManager } from '../AudioManager';
+import { TileObject } from '../objects/TileObject';
 export class TestLevel extends BasicLevel {
     constructor() {
         super();
         AudioManager.playMusic("assets/music/begins.ogg");
         
         this.objects.push(new PlayerObject(32, 512));
-        for (let i = 0; i < 25; i++) {
-            this.objects.push(new ImageObject("ground" + i, i * 32, 576, 32, 32, "assets/textures/objects/sprFallingBlock.png"));
-        }
-        this.objects.push(new ImageObject("ground25", 256, 544, 32, 32, "assets/textures/objects/sprFallingBlock.png"));
-        this.objects.push(new ImageObject("ground26", 256, 512, 32, 32, "assets/textures/objects/sprFallingBlock.png"));
-        this.objects.push(new ImageObject("ground27", 256, 480, 32, 32, "assets/textures/objects/sprFallingBlock.png"));
-        this.objects.push(new ImageObject("ground28", 256, 448, 32, 32, "assets/textures/objects/sprFallingBlock.png"));
-        this.objects.push(new ImageObject("ground29", 288, 416, 32, 32, "assets/textures/objects/sprFallingBlock.png"));
-        this.objects.push(new ImageObject("ground30", 288, 384, 32, 32, "assets/textures/objects/sprFallingBlock.png"));
+        this.objects.push(new TileObject("ground0", 0, 18, 25, 1, ["assets/textures/objects/sprFallingBlock.png"]));
+        this.objects.push(new TileObject("wall0", 8, 14, 1, 4, ["assets/textures/objects/sprFallingBlock.png"]));
+        this.objects.push(new TileObject("wall1", 9, 12, 1, 6, ["assets/textures/objects/sprFallingBlock.png"]));
 
         this.objects.push(new SpikeObject("spike0", 320, 416, 1));
         this.objects.push(new SpikeObject("spike1", 320, 384, 1));
---
+++ src/objects/TileObject.ts
@@ -0,0 +1,36 @@
+import { ImageObject } from "./ImageObject";
+export class TileObject extends ImageObject {
+    public otherImages: HTMLImageElement[];
+    public totalW: number;
+    public totalH: number;
+    public order: number[] = new Array(0);
+
+    constructor(id: string, x: number, y: number, w: number, h: number, sources: string[], order?: number[]) {
+        super(id, x * 32, y * 32, w * 32, h * 32, sources.shift());
+        this.image.width = 32;
+        this.image.height = 32;
+        this.totalW = w;
+        this.totalH = h;
+        this.otherImages = sources.map(src => {
+            let img: HTMLImageElement = new Image(32, 32);
+            img.src = src;
+            return img;
+        });
+        if (typeof order !== 'undefined') this.order = order;
+    }
+
+    public draw(ctx: CanvasRenderingContext2D, delta: number): void {
+        ctx.drawImage(this.image, this.polygon.pos.x, this.polygon.pos.y, 32, 32);
+        for (let i = 1; i < this.totalW * this.totalH; i++) {
+            ctx.drawImage(i < this.order.length ? this.otherImages[this.order[i]] : this.image,
+                this.polygon.pos.x + (i % this.totalW) * 32, this.polygon.pos.y + Math.floor(i / this.totalW) * 32, 32, 32);
+        }
+    }
+
+    public dispose(): void {
+        super.dispose();
+        while(this.otherImages.length != 0) {
+            this.otherImages.shift().remove();
+        }
+    }
+}
